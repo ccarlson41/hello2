dc5e613d2a88d8634934e62dcce038de21e3a93b
Cameron Carlson <ccarlson41@huskers.unl.edu>
2023-08-24 16:18:40 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author:  CAM
+ *
+ * A simple program that prints the goat's name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("The one and only: Cam Carlson!!!!!!!!\n");
+
+  return 0;
+}
